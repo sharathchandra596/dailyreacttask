0903417b460eb1f334d5f9acdf0d3979b9008e2a
sharathchandra596 <sharathjoshi596@gmail.com>
2024-04-15 14:28:22 +0530

    task of 15/04

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
+import { useState } from "react";
+import { useEffect } from "react";
 
 
 
 
 
 function App() {
-  return (
+const[datalength,setDataLength]=useState([])
+const[count,setCount]=useState(1)
+const[product,setProduct]=useState({})
+  useEffect(()=>{
+    fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(data=> {
+              setDataLength(data)
+              // console.log(data)
+            })
+           
+  },[])
+  // console.log(datalength)
+  useEffect(()=>{
+    fetch(`https://fakestoreapi.com/products/${count}`)
+            .then(res=>res.json())
+            .then(data=> {
+              setProduct(data)
+              // console.log(product)
+            })
+           
+  },[count])
+  console.log(product)
+  function handleClick(e)
+  {
     
-    <h1>task15/04</h1>
+    setCount(Number(e.target.value))
+    
+  }
+
+  return (
+    <>
+    <h1>task15/04</h1> 
+    <img src={product.image} alt={product.title} height={"80px"} width={"80px"}/>
+    <br/>
+
+    {datalength.map(item=> <button style={{height:"80px",width:"80px"}} onClick={handleClick} key={item.id} value={item.id}>{item.id} </button>)}
+
+    </>
   );
 }
 
